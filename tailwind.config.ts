fbace8012228d5198f0b1579fb794b389c926839
ACJT123 <acjtacjt@gmail.com>
2024-08-30 11:58:53 +0800

    feat(tailwind): add custom colors and responsive size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        white: "#E5E5E5",
+        lightBlack: "#CCCCCC",
+        gray: "#777777",
+        black: "#121022",
+        blue: "#251977",
+        green: "#62D85F",
+        red: "#EF5555",
+      },
+      screens: {
+        "4xs": "320px", // 4 extra small devices (smallest phones)
+        "3xs": "360px", // 3 extra small devices (larger phones)
+        "2xs": "400px", // 2 extra small devices (larger phones)
+        xs: "480px", // Extra small devices (small tablets or larger phones)
+        sm: "640px", // Small devices (tablets)
+        md: "768px", // Medium devices (tablets and small laptops)
+        lg: "1024px", // Large devices (desktops)
+        xl: "1280px", // Extra large devices (large desktops)
+        "2xl": "1536px", // Extra extra large devices (very large desktops)
+      },
     },
   },
   plugins: [],
